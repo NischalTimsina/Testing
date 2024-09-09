21a66189ef77f5fd29a82b5ec366775cdb075b32
NischalTimsina <82948136+NischalTimsina@users.noreply.github.com>
2024-09-09 08:07:25 +0545

    Add files via upload

---
+++ scripts/form-103f.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'file:&#x2F;&#x2F;&#x2F;C:&#x2F;Users&#x2F;USER&#x2F;AppData&#x2F;Local&#x2F;Temp&#x2F;Nicepage&#x2F;chrome&#x2F;index.html',
+    'to' => 'timsina.nischal65@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
